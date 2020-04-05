940174dc547e6d3704cb9c7277845dd156449cab
Alejandro Hernández <alejandrehl@icloud.com>
2020-04-05 12:51:29 -0400

    se agrego el decorador de class validator en el get tasks filter dto

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,10 @@
 import { TaskStatus } from './../task.model';
+import { IsOptional } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsOptional()
   status: TaskStatus;
+
+  @IsOptional()
   search: string;
 }
