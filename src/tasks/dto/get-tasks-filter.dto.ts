b6e5f344f7e18eab0160d9b36d93d35b8e092673
Alejandro Hernández <alejandrehl@icloud.com>
2020-04-05 12:52:43 -0400

    update get tasks filter DTO

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,10 +1,12 @@
 import { TaskStatus } from './../task.model';
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
 export class GetTasksFilterDto {
   @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
   status: TaskStatus;
 
   @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
