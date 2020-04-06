2e3caba6aa7188e10b2639d3968b74a6929cb35f
Alejandro Hernández <alejandrehl@icloud.com>
2020-04-05 21:14:40 -0400

    update tasks status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -41,13 +41,13 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  // @Patch(':id/status')
-  // updateTaskStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  // ) {
-  //   return this.tasksService.updateTaskStatus(id, status);
-  // }
+  @Patch(':id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
 
   @Delete(':id')
   deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
