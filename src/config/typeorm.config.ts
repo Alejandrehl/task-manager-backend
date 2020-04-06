97199c47bd4ce57303c4410f2c7913d6980f7070
Alejandro Hernández <alejandrehl@icloud.com>
2020-04-06 14:45:11 -0400

    Add typeorm_sync typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,5 +11,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  synchronize: dbConfig.synchronize,
+  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };
