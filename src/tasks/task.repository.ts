bd1a75083e521c374b3bdebfbdf4e47a5e8c421b
Alejandro Hernández <alejandrehl@icloud.com>
2020-04-05 21:39:34 -0400

    Get all tasks by search done

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -10,12 +10,12 @@ export class TaskRepository extends Repository<Task> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
 
-    if (status) {
-      query.andWhere('task.status = :status', { status });
-    }
-
-    if (search) {
-    }
+    if (status) query.andWhere('task.status = :status', { status });
+    if (search)
+      query.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        { search: `%${search}%` },
+      );
 
     const tasks = await query.getMany();
     return tasks;
