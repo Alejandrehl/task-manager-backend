6bb247eb9a29d7934c951f074c7f8e843a8cfe8d
Alejandro Hernández <alejandrehl@icloud.com>
2020-04-06 17:22:34 -0400

    avances en tests

---
+++ src/example.spec.ts
@@ -0,0 +1,25 @@
+class FriendList {
+  friends = [];
+
+  addFriend(name) {
+    this.friends.push(name);
+    this.announceFriendship(name);
+  }
+
+  announceFriendship(name) {
+    global.console.log(`${name} is now friend!`);
+  }
+}
+
+describe('FriendsList', () => {
+  it('initializes friends list', () => {
+    const friendList = new FriendList();
+    expect(friendList.friends.length).toEqual(0);
+  });
+
+  it('add a friend to the list', () => {
+    const friendList = new FriendList();
+    friendList.addFriend('Alejandro');
+    expect(friendList.friends.length).toEqual(1);
+  });
+});
