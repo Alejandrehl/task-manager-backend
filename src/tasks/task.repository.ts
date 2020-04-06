53638b663612ec6e331b922f076555ccd0f1af1e
Alejandro Hernández <alejandrehl@icloud.com>
2020-04-06 13:52:47 -0400

    Logger in task repository

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -4,9 +4,12 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { Repository, EntityRepository } from 'typeorm';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
+import { Logger, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  private logger = new Logger('TaskRepository');
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -20,8 +23,18 @@ export class TaskRepository extends Repository<Task> {
         { search: `%${search}%` },
       );
 
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (err) {
+      this.logger.error(
+        `Failed to get tasks for user "${
+          user.username
+        }", Filters: ${JSON.stringify(filterDto)}`,
+        err.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
@@ -32,9 +45,18 @@ export class TaskRepository extends Repository<Task> {
     task.description = description;
     task.status = TaskStatus.OPEN;
     task.user = user;
-    await task.save();
-    delete task.user;
 
+    try {
+      await task.save();
+    } catch (err) {
+      this.logger.error(
+        `Failed to create task for user "${user.username}. Data: ${createTaskDto}`,
+        err.stack,
+      );
+      throw new InternalServerErrorException();
+    }
+
+    delete task.user;
     return task;
   }
 }
