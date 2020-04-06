df41242d7478ba16b960f1ca7dcc734ec0f648d9
Alejandro Hernández <alejandrehl@icloud.com>
2020-04-05 23:48:21 -0400

    Retornando correctamente access token cuando las credenciales son válidas

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,35 @@
+import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return await this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const user = await this.userRepository.validateUserPassword(
       authCredentialsDto,
     );
 
     if (!user) throw new UnauthorizedException('Invalid credentials.');
-    return user;
+
+    const payload: JwtPayload = user;
+    const accessToken = await this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
